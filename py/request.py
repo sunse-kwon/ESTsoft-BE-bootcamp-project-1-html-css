ed7711b516dcb21604a658b35dbd0c07e7a6c50c
Sunse Kwon <sunse523@gmail.com>
2023-06-02 19:25:18 +0900

    request.py modified and communication seemed fixed but unsure yet

--- py/request.py
+++ py/request.py
@@ -1,10 +1,12 @@
 from pyodide.http import pyfetch, FetchResponse
 from typing import Optional, Any
 
-async def request(url: str, method: str = "POST", body: Optional[str] = None,
-                headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
-    kwargs = {"method": method, "mode": "cors"}  # CORS: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
-    if body and method not in ["POST", "HEAD"]:
+
+async def request(url: str, method: str = "GET", body: Optional[str] = None,
+                  headers: Optional[dict[str, str]] = None, **fetch_kwargs: Any) -> FetchResponse:
+    # CORS: https://en.wikipedia.org/wiki/Cross-origin_resource_sharing
+    kwargs = {"method": method, "mode": "cors"}
+    if body and method not in ["GET", "HEAD"]:
         kwargs["body"] = body
     if headers:
         kwargs["headers"] = headers
