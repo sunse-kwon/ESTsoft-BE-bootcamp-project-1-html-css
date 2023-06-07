2f07f111c600c1561ffd27c7a5c8b7563f20396d
Sunse Kwon <sunse523@gmail.com>
2023-06-07 18:07:33 +0900

    form datas transmission successfully done and added pre-training dataset as an criterias to generate answer

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function handleSubmit () {
+    const gender  = document.querySelector('input[name="gender"]:checked').value;
+    const transports  = document.querySelectorAll('input[type="checkbox"]:checked')
+    let output = []
+    transports.forEach((item) => {
+        output.push(item.value)
+    } );
+    const travelStyle  = document.querySelector('input[name="travel-style"]:checked').value;
+    const departure  = document.querySelector('input[name="departure"]').value;
+    const arrival  = document.querySelector('input[name="arrival"]').value;
+    const numPeople  = document.querySelector('input[name="num-people"]').value;
+    const budget  = document.querySelector('input[name="budget"]').value;
+
+    sessionStorage.setItem('gender',gender )
+    sessionStorage.setItem('transports',output )
+    sessionStorage.setItem('travelStyle',travelStyle )
+    sessionStorage.setItem('departure',departure )
+    sessionStorage.setItem('arrival',arrival )
+    sessionStorage.setItem('numPeople',numPeople )
+    sessionStorage.setItem('budget',budget )
+
+    return ;
+}
+
+
+
